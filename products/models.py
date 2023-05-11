88ed9a8757361a705d8f1f03ef8f9c399c6e993f
Ahmed Mustafa <61328457+devAhmedMustafa@users.noreply.github.com>
2023-05-11 15:30:21 +0300

    Branching templates

--- products/models.py
+++ products/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from users.models import CustomUser as User
 
-# Create your models here.
+class Category(models.Model):
+
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000, null=True)
+    price = models.FloatField()
+    on_stock = models.BooleanField(default=True)
+    left = models.IntegerField()
+    image = models.ImageField(upload_to='images/%y/%m/%d', null=True)
+    details = models.JSONField(null=True)
+    discount = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.category.name}: {self.name}"
+
+
+class Rating(models.Model):
+
+    stars = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Product
+import json
 # Create your views here.
 
 
@@ -8,4 +9,13 @@ def home(request):
     
     user = request.user
     
+
     return HttpResponse(f'Hello{user.first_name}')
+
+def products(request):
+
+    products = Product.objects.get(name = 'IPhone 6')
+
+    details = products.details
+
+    return render(request, 'products/products.html', {'products': products, 'details': details.items})
